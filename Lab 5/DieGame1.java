99b0391e524118f5ee7281539672f7772edb0ce3
Liam Nugent <liamnnugent@gmail.com>
2023-03-09 19:11:33 +0000

    Added comments to DieGame1.java/Die.java

--- Lab 5/DieGame1.java
+++ Lab 5/DieGame1.java
@@ -1,8 +1,10 @@
 public class DieGame1 {
   public static void main(String[] args) {
 
+    // Create a die with 6 sides
     Die die1 = new Die(6);
 
+    // Set initial score to 5 and initialize flag that game is not over
     int score = 5;
     boolean gameOver = false;
 
@@ -15,13 +17,18 @@ public class DieGame1 {
       die1.throwDie();
       System.out.println(die1.getFaceValue() + "\t\t" + score);
       if (die1.getFaceValue() == 6) {
+        // If 6 is rolled, end the game and display a win message with the final
+        // score
         gameOver = true;
         System.out.println("\nYou've thrown a six! You win with a score of " +
                            score + "!\n");
       } else if (score < 1) {
+        // If 6 isn't rolled before the score reaches 0, end the game and
+        // display a lose message
         gameOver = true;
         System.out.println("\nYou lose!\n");
       }
+      // Decrement the score on each turn
       score--;
     }
   }
--- Lab 5/die.java
+++ Lab 5/die.java
@@ -11,20 +11,27 @@ public class Die {
   // Random number generator for die
   private Random generator;
 
-  // Default constructor
+  // Default constructor that takes in number of sides as parameter
   public Die(int s) {
     sides = s;
+    // initialize Random object
     generator = new Random();
+    // generate random face value between 1 and number of sides
     faceValue = generator.nextInt(sides) + 1;
   }
 
+  // Method to generate and set a new random face value of the die
   public void throwDie() {
+    // create new Random object
     generator = new Random();
+    // generate random face value between 1 and number of sides
     faceValue = generator.nextInt(sides) + 1;
   }
 
+  // Get method for the current face value of the die
   public int getFaceValue() { return faceValue; }
 
+  // String representation of the Die object
   public String toString() {
     return getClass().getName() + "[faceValue=" + getFaceValue() +
         ", sides=" + sides + "]";
