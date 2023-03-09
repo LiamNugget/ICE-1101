3dc27742c5e05065e36f601eb0802d9fff27fd99
Liam Nugent <liamnnugent@gmail.com>
2023-03-09 16:35:02 +0000

    Die class updates, toString formmated correctly

---
+++ Lab 5/DieTester.java
@@ -0,0 +1,9 @@
+public class DieTester {
+  public static void main(String[] args) {
+    // Create a die with 6 sides
+    Die die1 = new Die(6);
+
+    // Create a tostring method in the Die class
+    System.out.println(die1);
+  }
+}
